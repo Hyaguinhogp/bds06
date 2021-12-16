7568977d04b066720b3f171af0e2c7ca55a066a6
Hyago Matos <hyago.v.a.m@gmail.com>
2021-12-16 18:46:50 -0400

    User CRUD

---
+++ src/main/java/com/devsuperior/movieflix/controllers/UserController.java
@@ -0,0 +1,26 @@
+package com.devsuperior.movieflix.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.movieflix.entities.User;
+import com.devsuperior.movieflix.services.UserService;
+
+@RestController
+@RequestMapping(path = "/users")
+public class UserController {
+
+	@Autowired
+	private UserService userService;
+	
+	@GetMapping
+	public ResponseEntity<List<User>> getUsers(){
+		List<User> users = userService.getUsers();
+		return ResponseEntity.ok().body(users);
+	}
+}
